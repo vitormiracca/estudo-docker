be09e1c731d5a75e2afb8139d86344441dd4a383
Vitor Miracca <vitormiracca@gmail.com>
2024-09-28 14:58:34 -0300

    feat: app simples funfando

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 # Arquivo python para testes
+from numpy import amin
+import streamlit as st
+
+def hello_world() -> str:
+    return "Hello, Docker!"
+
+def main():
+    st.write(hello_world())
+
+if __name__ == "__main__":
+    main()
